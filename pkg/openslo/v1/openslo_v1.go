1d65da816074335c1f0e30114c0dd168c61bcd19
Ibrahim Diallo <ibrahim.diallo@hpe.com>
2024-03-28 01:14:59 -0400

    generate json functions

--- pkg/openslo/v1/openslo_v1.go
+++ pkg/openslo/v1/openslo_v1.go
@@ -1,3 +1,118 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse and unparse this JSON data, add this code to your project and do:
+//
+//    alertCondition, err := UnmarshalAlertCondition(bytes)
+//    bytes, err = alertCondition.Marshal()
+//
+//    alertNotificationTarget, err := UnmarshalAlertNotificationTarget(bytes)
+//    bytes, err = alertNotificationTarget.Marshal()
+//
+//    alertPolicy, err := UnmarshalAlertPolicy(bytes)
+//    bytes, err = alertPolicy.Marshal()
+//
+//    budgetAdjustment, err := UnmarshalBudgetAdjustment(bytes)
+//    bytes, err = budgetAdjustment.Marshal()
+//
+//    datasource, err := UnmarshalDatasource(bytes)
+//    bytes, err = datasource.Marshal()
+//
+//    service, err := UnmarshalService(bytes)
+//    bytes, err = service.Marshal()
+//
+//    sLI, err := UnmarshalSLI(bytes)
+//    bytes, err = sLI.Marshal()
+//
+//    slo, err := UnmarshalSlo(bytes)
+//    bytes, err = slo.Marshal()
+
+package openslo_v1
+
+import "bytes"
+import "errors"
+import "time"
+
+import "encoding/json"
+
+func UnmarshalAlertCondition(data []byte) (AlertCondition, error) {
+	var r AlertCondition
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *AlertCondition) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalAlertNotificationTarget(data []byte) (AlertNotificationTarget, error) {
+	var r AlertNotificationTarget
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *AlertNotificationTarget) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalAlertPolicy(data []byte) (AlertPolicy, error) {
+	var r AlertPolicy
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *AlertPolicy) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalBudgetAdjustment(data []byte) (BudgetAdjustment, error) {
+	var r BudgetAdjustment
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *BudgetAdjustment) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalDatasource(data []byte) (Datasource, error) {
+	var r Datasource
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Datasource) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalService(data []byte) (Service, error) {
+	var r Service
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Service) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalSLI(data []byte) (SLI, error) {
+	var r SLI
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *SLI) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func UnmarshalSlo(data []byte) (Slo, error) {
+	var r Slo
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Slo) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
 // An Alert Condition allows you to define under which conditions an alert for an SLO needs
 // to be triggered.
 //
@@ -130,8 +245,6 @@ type BudgetAdjustment struct {
 	Metadata                                                                              Metadata             `json:"metadata"`
 	Spec                                                                                  BudgetAdjustmentSpec `json:"spec"`
 }
-
-import "time"
 
 type BudgetAdjustmentSpec struct {
 	Description  *string            `json:"description,omitempty"`
@@ -357,7 +470,150 @@ type Label struct {
 	String        *string
 }
 
+func (x *Label) UnmarshalJSON(data []byte) error {
+	x.AnythingArray = nil
+	object, err := unmarshalUnion(data, nil, nil, nil, &x.String, true, &x.AnythingArray, false, nil, false, nil, false, nil, false)
+	if err != nil {
+		return err
+	}
+	if object {
+	}
+	return nil
+}
+
+func (x *Label) MarshalJSON() ([]byte, error) {
+	return marshalUnion(nil, nil, nil, x.String, x.AnythingArray != nil, x.AnythingArray, false, nil, false, nil, false, nil, false)
+}
+
 type TimeSliceWindow struct {
 	Double *float64
 	String *string
+}
+
+func (x *TimeSliceWindow) UnmarshalJSON(data []byte) error {
+	object, err := unmarshalUnion(data, nil, &x.Double, nil, &x.String, false, nil, false, nil, false, nil, false, nil, false)
+	if err != nil {
+		return err
+	}
+	if object {
+	}
+	return nil
+}
+
+func (x *TimeSliceWindow) MarshalJSON() ([]byte, error) {
+	return marshalUnion(nil, x.Double, nil, x.String, false, nil, false, nil, false, nil, false, nil, false)
+}
+
+func unmarshalUnion(data []byte, pi **int64, pf **float64, pb **bool, ps **string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) (bool, error) {
+	if pi != nil {
+		*pi = nil
+	}
+	if pf != nil {
+		*pf = nil
+	}
+	if pb != nil {
+		*pb = nil
+	}
+	if ps != nil {
+		*ps = nil
+	}
+
+	dec := json.NewDecoder(bytes.NewReader(data))
+	dec.UseNumber()
+	tok, err := dec.Token()
+	if err != nil {
+		return false, err
+	}
+
+	switch v := tok.(type) {
+	case json.Number:
+		if pi != nil {
+			i, err := v.Int64()
+			if err == nil {
+				*pi = &i
+				return false, nil
+			}
+		}
+		if pf != nil {
+			f, err := v.Float64()
+			if err == nil {
+				*pf = &f
+				return false, nil
+			}
+			return false, errors.New("Unparsable number")
+		}
+		return false, errors.New("Union does not contain number")
+	case float64:
+		return false, errors.New("Decoder should not return float64")
+	case bool:
+		if pb != nil {
+			*pb = &v
+			return false, nil
+		}
+		return false, errors.New("Union does not contain bool")
+	case string:
+		if haveEnum {
+			return false, json.Unmarshal(data, pe)
+		}
+		if ps != nil {
+			*ps = &v
+			return false, nil
+		}
+		return false, errors.New("Union does not contain string")
+	case nil:
+		if nullable {
+			return false, nil
+		}
+		return false, errors.New("Union does not contain null")
+	case json.Delim:
+		if v == '{' {
+			if haveObject {
+				return true, json.Unmarshal(data, pc)
+			}
+			if haveMap {
+				return false, json.Unmarshal(data, pm)
+			}
+			return false, errors.New("Union does not contain object")
+		}
+		if v == '[' {
+			if haveArray {
+				return false, json.Unmarshal(data, pa)
+			}
+			return false, errors.New("Union does not contain array")
+		}
+		return false, errors.New("Cannot handle delimiter")
+	}
+	return false, errors.New("Cannot unmarshal union")
+
+}
+
+func marshalUnion(pi *int64, pf *float64, pb *bool, ps *string, haveArray bool, pa interface{}, haveObject bool, pc interface{}, haveMap bool, pm interface{}, haveEnum bool, pe interface{}, nullable bool) ([]byte, error) {
+	if pi != nil {
+		return json.Marshal(*pi)
+	}
+	if pf != nil {
+		return json.Marshal(*pf)
+	}
+	if pb != nil {
+		return json.Marshal(*pb)
+	}
+	if ps != nil {
+		return json.Marshal(*ps)
+	}
+	if haveArray {
+		return json.Marshal(pa)
+	}
+	if haveObject {
+		return json.Marshal(pc)
+	}
+	if haveMap {
+		return json.Marshal(pm)
+	}
+	if haveEnum {
+		return json.Marshal(pe)
+	}
+	if nullable {
+		return json.Marshal(nil)
+	}
+	return nil, errors.New("Union must not be null")
 }
